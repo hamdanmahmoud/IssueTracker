4d16760084290d98cbb55e2b9778ead2dad5fbd2
Mahmoud <hamdan.mahmoudtudor@gmail.com>
2020-09-28 23:16:34 +0300

    cleaned up ish

--- src/app/layouts/core-layout/project/project-page/project-page.component.ts
+++ src/app/layouts/core-layout/project/project-page/project-page.component.ts
@@ -20,31 +20,6 @@ export class ProjectPageComponent implements OnInit {
   displayedIssue: Issue;
   statusList: IssueStatus[];
 
-  folders: Section[] = [
-    {
-      name: "Photos",
-      updated: new Date("1/1/16"),
-    },
-    {
-      name: "Recipes",
-      updated: new Date("1/17/16"),
-    },
-    {
-      name: "Work",
-      updated: new Date("1/28/16"),
-    },
-  ];
-  notes: Section[] = [
-    {
-      name: "Vacation Itinerary",
-      updated: new Date("2/20/16"),
-    },
-    {
-      name: "Kitchen Remodel",
-      updated: new Date("1/18/16"),
-    },
-  ];
-
   constructor() {}
 
   ngOnInit(): void {
