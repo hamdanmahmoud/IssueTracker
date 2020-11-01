b94d6b031d8b3b79210e10b6dd4a957b7b6a10fa
Mahmoud <hamdan.mahmoudtudor@gmail.com>
2020-11-01 13:03:30 +0200

    done

--- src/app/API.conf.ts
+++ src/app/API.conf.ts
@@ -1,7 +1,15 @@
+// export class API {
+//   static aclURL: string = "http://localhost:80/acl";
+//   static userURL: string = "http://localhost:80/user";
+//   static projectURL: string = "http://localhost:80/project";
+//   static issueURL: string = "http://localhost:80/issue";
+//   static authURL: string = "http://localhost:80/auth";
+// }
+
 export class API {
-  static aclURL: string = "http://localhost:8080";
-  static userURL: string = "http://localhost:8081";
-  static projectURL: string = "http://localhost:8082";
-  static issueURL: string = "http://localhost:8083";
-  static authURL: string = "http://localhost:8084";
+  static aclURL: string = "http://34218b2173a9.ngrok.io/acl";
+  static userURL: string = "http://34218b2173a9.ngrok.io/user";
+  static projectURL: string = "http://34218b2173a9.ngrok.io/project";
+  static issueURL: string = "http://34218b2173a9.ngrok.io/issue";
+  static authURL: string = "http://34218b2173a9.ngrok.io/auth";
 }
