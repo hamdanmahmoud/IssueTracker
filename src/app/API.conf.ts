7cb28f8dfa69c515350db5771ce93712fc842394
Mahmoud <hamdan.mahmoudtudor@gmail.com>
2020-11-07 09:25:27 +0200

    now direct tunneling through port fw

--- src/app/API.conf.ts
+++ src/app/API.conf.ts
@@ -6,10 +6,23 @@
 //   static authURL: string = "http://localhost:80/auth";
 // }
 
+// export class API {
+//   static aclURL: string = "https://cd2bf96639e1.ngrok.io/acl";
+//   static userURL: string = "https://cd2bf96639e1.ngrok.io/user";
+//   static projectURL: string = "https://cd2bf96639e1.ngrok.io/project";
+//   static issueURL: string = "https://cd2bf96639e1.ngrok.io/issue";
+//   static authURL: string = "https://cd2bf96639e1.ngrok.io/auth";
+// }
+
 export class API {
-  static aclURL: string = "https://cd2bf96639e1.ngrok.io/acl";
-  static userURL: string = "https://cd2bf96639e1.ngrok.io/user";
-  static projectURL: string = "https://cd2bf96639e1.ngrok.io/project";
-  static issueURL: string = "https://cd2bf96639e1.ngrok.io/issue";
-  static authURL: string = "https://cd2bf96639e1.ngrok.io/auth";
+  static aclURL: string =
+    "https://[2a04:241e:101:8100:48dc:fb65:3e7c:6c48]/acl";
+  static userURL: string =
+    "https://[2a04:241e:101:8100:48dc:fb65:3e7c:6c48]/user";
+  static projectURL: string =
+    "https://[2a04:241e:101:8100:48dc:fb65:3e7c:6c48]/project";
+  static issueURL: string =
+    "https://[2a04:241e:101:8100:48dc:fb65:3e7c:6c48]/issue";
+  static authURL: string =
+    "https://[2a04:241e:101:8100:48dc:fb65:3e7c:6c48]/auth";
 }
