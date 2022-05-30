5885cdf752941b7840cd983db40d6c6a75b8303e
mahamdan <mahamdan@cisco.com>
2022-05-30 16:06:39 +0300

    Updated API endpoints to local ones, upgraded lombok backend dependency to avoid this issue https://stackoverflow.com/questions/66801256/java-lang-illegalaccesserror-class-lombok-javac-apt-lombokprocessor-cannot-acce

--- ui/src/app/API.conf.ts
+++ ui/src/app/API.conf.ts
@@ -1,23 +1,7 @@
-// export class API {
-//   static aclURL: string = "http://localhost:80/acl";
-//   static userURL: string = "http://localhost:80/user";
-//   static projectURL: string = "http://localhost:80/project";
-//   static issueURL: string = "http://localhost:80/issue";
-//   static authURL: string = "http://localhost:80/auth";
-// }
-
-// export class API {
-//   static aclURL: string = "https://cd2bf96639e1.ngrok.io/acl";
-//   static userURL: string = "https://cd2bf96639e1.ngrok.io/user";
-//   static projectURL: string = "https://cd2bf96639e1.ngrok.io/project";
-//   static issueURL: string = "https://cd2bf96639e1.ngrok.io/issue";
-//   static authURL: string = "https://cd2bf96639e1.ngrok.io/auth";
-// }
-
 export class API {
-  static aclURL: string = "https://issuetracker.club/acl";
-  static userURL: string = "https://issuetracker.club/user";
-  static projectURL: string = "https://issuetracker.club/project";
-  static issueURL: string = "https://issuetracker.club/issue";
-  static authURL: string = "https://issuetracker.club/auth";
+  static aclURL: string = "http://localhost:8080/acl";
+  static userURL: string = "http://localhost:8081/user";
+  static projectURL: string = "http://localhost:8082/project";
+  static issueURL: string = "http://localhost:8083/issue";
+  static authURL: string = "http://localhost:8084/auth";
 }
